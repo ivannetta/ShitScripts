2d9959ad7cedca34c47cf5fa971244454a8be06c
Ivan Fila <opr103osos@gmail.com>
2025-04-22 19:49:34 +0100

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,3 +1,7 @@
+if string.split(identifyexecutor() or "None", " ")[1] == "Xeno" then
+    getgenv().WebSocket = nil
+end
+
 local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
 api.script_id = "d0632c30c8af0316aae11552dcb2a21d"
 local sigmakey
