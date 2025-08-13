0de54b2121cc0bf8b1579b915e97b7efe9ce0b8f
Ivan Fila <opr103osos@gmail.com>
2025-08-13 21:29:32 +0100

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -1,2 +1,21 @@
 setclipboard("discord.gg/fartsaken | https://imperium.mysellauth.com")
-game.Players.LocalPlayer:Kick("Unfortunately, i havent been making enough money from fartsaken to pay for luarmor, if you want to keep using the script. you will have to either wait for me to get money, or buy a key for 1$ a week or 6$ lifetime. \n im sorry, the discord invite is copied to your clipboard along with the shop link")
+local P = game.Players.LocalPlayer
+local PG = P:WaitForChild("PlayerGui")
+
+local SG = Instance.new("ScreenGui", PG)
+local FR = Instance.new("Frame", SG)
+FR.Size = UDim2.new(1, 0, 1, 0)
+FR.Position = UDim2.new(0, 0, 0, 0)
+FR.BackgroundColor3 = Color3.new(0, 0, 0)
+
+local TL = Instance.new("TextLabel", FR)
+TL.Size = UDim2.new(1, 0, 1, 0)
+TL.Position = UDim2.new(0, 0, 0, 0)
+TL.BackgroundTransparency = 1
+TL.TextColor3 = Color3.new(1, 1, 1)
+TL.TextScaled = true
+TL.Font = Enum.Font.SourceSans
+TL.Text = "Unfortunately, I haven't been making enough money from fartsaken to pay for luarmor. If you want to keep using the script, you will have to either wait for me to get money, or buy a key for $1 a week or $6 lifetime.\n\nSorry."
+
+task.wait(25)
+P:Kick("gn")
