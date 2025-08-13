1e573eb1e77cabcd4311c92f37c4ce599d4fe52a
Ivan Fila <opr103osos@gmail.com>
2025-08-13 21:30:01 +0100

    Update forsaken.lua

--- forsaken.lua
+++ forsaken.lua
@@ -15,7 +15,7 @@ TL.BackgroundTransparency = 1
 TL.TextColor3 = Color3.new(1, 1, 1)
 TL.TextScaled = true
 TL.Font = Enum.Font.SourceSans
-TL.Text = "Unfortunately, I haven't been making enough money from fartsaken to pay for luarmor. If you want to keep using the script, you will have to either wait for me to get money, or buy a key for $1 a week or $6 lifetime.\n\nSorry."
+TL.Text = "Unfortunately, I haven't been making enough money from fartsaken to pay for luarmor. If you want to keep using the script, you will have to either wait for me to get money, or buy a key for $1 a week or $6 lifetime.\n\nSorry. \n discord.gg/fartsaken copied to clipboard the discord invite and shop link"
 
 task.wait(25)
 P:Kick("gn")
